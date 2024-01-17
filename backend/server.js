b45a1f9acab90f20f44ff060011b664ea985c44c
Akoatem <akoatemsam2003@gmail.com>
2024-01-17 16:34:01 +0200

    connect to DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
-import expres from "express"
+import express from "express"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import connectDB from "./config/db.js"
+import cors from "cors"
 
-const app = expres()
+// configure env
+dotenv.config()
+// database config
+connectDB()
+
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+const PORT = process.env.PORT || 8080
 
 
-app.listen(8000, ()=>{
-   console.log("server listening on 8000");
+app.listen(PORT, ()=>{
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white)
 })
